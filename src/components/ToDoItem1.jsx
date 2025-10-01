a34b0b187341ebac0b850da34a3a7517c3f5d191
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-10-01 12:06:30 +0530

    work on schedule

--- src/components/ToDoItem1.jsx
+++ src/components/ToDoItem1.jsx
@@ -3,7 +3,7 @@ function ToDoItem1() {
   let tododt = "09/01/2027 03:00 AM";
 
   return (
-    <div class="container">
+    <div class='container'>
       <div class="row td-row">
         <div class="col-2">{tododt}</div> 
         <div class="col">{todotask}</div>
