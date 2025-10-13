a4537b5b779dd27315fc3c49d2c95650ed868304
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-10-13 17:19:33 +0530

    one more thing added to list

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AddToDo from "./components/AddToDo";
 import AppName from "./components/AppName";
 import ToDoItem from "./components/ToDoItem";
 import ToDoItem1 from "./components/ToDoItem1";
+import ToDoItem2 from "./components/ToDoItem2";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div class="list-container">
         <ToDoItem />
         <ToDoItem1 />
+        <ToDoItem2 />
       </div>
     </center>
   );
---
+++ src/components/ToDoItem2.jsx
@@ -0,0 +1,20 @@
+function ToDoItem2() {
+  let todotask = "Fitness Time";
+  let tododt = "14/10/2025 06:00 AM";
+
+  return (
+    <div class="container">
+      <div class="row td-row">
+        <div class="col-2"> {tododt} </div>
+        <div class="col"> {todotask} </div>
+        <div class="col-md-auto">
+          <button type="button" class="btn btn-danger t-button">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ToDoItem2;
