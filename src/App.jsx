35422072d6725d7ad8decc523c757b472c7c6a04
DATLARAVINDRABHUPATHIVARMA <ravivarmadatla64@gmail.com>
2025-12-10 23:42:10 -0600

    TO DO Version

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
     { todotask: "Ready and Refresh", todofrom: "10/12/2025 08:00 AM", todoto: "09/12/2025 09:00 AM" },
     { todotask: "Breakfast Time", todofrom: "10/12/2025 09:00 AM", todoto: "10/12/2025 09:30 AM" },
     { todotask: "Office Work", todofrom: "10/12/2025 09:30 AM", todoto: "10/12/2025 10:00 AM" },
+    { todotask: "Team Meeting", todofrom: "10/12/2025 10:00 AM", todoto: "10/12/2025 11:00 AM" },
   ]
 
   return (
